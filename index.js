44806c0f8c16b25cb3be6205135ceb2c3b8b0f31
Vida Owusu <vidakumiwaaowusu@gmail.com>
2024-06-26 16:15:45 +0000

    Express middleware

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 import express from "express";
-import recipeRouter from "./routes/recipes.js";
+import mongoose from "mongoose";
+import recipeRouter from "./routes/recipe.js";
+
+//Connect to database
+await mongoose.connect(process.env.MONGO_URL);
+
 
 //Create Express App
 const app = express();
 
+//Apply middlewares
+app.use(express.json());
+
 //Define routes
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.json('Welcome to Backend Vidash');
 });
 
-app.post('/login', (req, res)=>{
+app.post('/login', (req, res) => {
     res.json('Login successful');
 });
 
-app.patch('/menu', (req, res)=>{
+app.patch('/menu', (req, res) => {
     res.json('All sumptous foods are available');
 });
-app.patch('/about', (req, res)=>{
+app.patch('/about', (req, res) => {
     res.json('Customer satisfaction is our hallmark');
 });
 
@@ -28,3 +36,10 @@ app.use(recipeRouter);
 app.listen(3000, () => {
     console.log('App listening on port 3000');
 });
+
+
+
+
+
+
+//2AMWFJO3eIPl4axV
